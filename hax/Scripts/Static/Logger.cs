ff24ca221bf60cf2a15c653b9460efff5aba3fd6
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2023-12-09 17:41:54 +0800

    feat: logger now stores timestamp

--- hax/Scripts/Static/Logger.cs
+++ hax/Scripts/Static/Logger.cs
@@ -2,12 +2,17 @@ using System;
 using System.IO;
 
 public static class Logger {
-    const string logFileName = "hax.log";
+    const string logFileName = "lc-hax.log";
     static object LockObject { get; } = new();
 
     public static void Write(string message) {
         lock (Logger.LockObject) {
-            File.AppendAllText(logFileName, $"{message}{Environment.NewLine}");
+            string timeNow = DateTime.Now.ToString("dd-MM-yy HH:mm:ss");
+
+            File.AppendAllText(
+                logFileName,
+                $"[{timeNow}] {message}{Environment.NewLine}"
+            );
         }
     }
 }
